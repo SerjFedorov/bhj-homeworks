13eef81adeed51ddff1a36f446ea987e94ee0af1
Sergey Fedorov <fedyaser@inbox.ru>
2023-04-15 06:51:21 +0700

    version 1, 2 task

---
+++ js-features/countdown/task.js
@@ -0,0 +1,50 @@
+const element = document.getElementById('timer');
+
+let counter = setInterval( () => {
+    let start = element.textContent; // textContent получает содержимое всех элементов, включая <script> и <style>, тогда как innerText этого не делает.
+    //innerText умеет считывать стили и не возвращает содержимое скрытых элементов, тогда как textContent это делает.
+    //Метод innerText позволяет получить CSS, а textContent — нет.
+    --start;
+    element.textContent = start;   
+    if (start === 0) {
+        alert('Вы победили в конкурсе!'); 
+        clearInterval(counter);            
+    }         
+}, 1);
+
+
+
+
+
+
+const sec = document.getElementById('sec');
+const min = document.getElementById('min');
+const hours = document.getElementById('hours');
+
+
+let counterTimer = setInterval( () => {
+    let startTimer = sec.textContent;
+    --startTimer;  
+    sec.textContent = startTimer;
+    if (hours.textContent == 0 && min.textContent == 0 && sec.textContent == 0) {
+        alert('Время вышло!'); 
+        clearInterval(counterTimer);
+    }        
+    if (startTimer == 0 ){
+        sec.textContent = 59;
+        min.textContent = min.textContent - 1; 
+        if (min.textContent == 0) {
+            min.textContent = 59;
+            hours.textContent = hours.textContent -1;
+            if (hours.textContent < 10 & hours.textContent >= 0) {
+                hours.textContent = '0' + hours.textContent;    
+                }                  
+            }
+        if (min.textContent < 10 & min.textContent > 0) {
+            min.textContent = '0' + min.textContent;    
+        }         
+    }           
+    if (startTimer < 10 & startTimer > 0) {
+        sec.textContent = '0' + startTimer;    
+    }      
+}, 10);
